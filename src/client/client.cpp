b1b2d2e3e72b6b3169cb011f47a8f3c1b660e805
TheoVerhelst <tverhels@ulb.ac.be>
2016-02-13 15:03:03 +0100

    Rewrite client.cpp which was loose in merge
    Accidentally, the file src/client/client.cpp of the branch
    BasicMenuManagement was replaced by the one of master, which is empty.
    This file is now rewritten as in BasicMenuManagement.

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include "client/StateStack.hpp"
+#include "client/states/HomeState.hpp"
+
 int main()
 {
+	StateStack stack;
+	stack.push<HomeState>();
+	while(not stack.isEmpty())
+	{
+		system("clear");
+		stack.display();
+		std::cout << "What do you want to do? ";
+		std::string input;
+		std::getline(std::cin, input);
+		stack.handleInput(input);
+	}
 	return 0;
 }
