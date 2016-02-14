43f3459af74910bc5758ca39eae10ac6e952410c
NBaudoux <nbaudoux@ulb.ac.be>
2016-02-14 14:04:22 +0000

    Moving card.hpp

--- include/common/card.hpp
+++ include/common/card.hpp
@@ -2,24 +2,28 @@
 #define _CARD_COMMON_HPP
 
 #include <string>
-#include "effect.hpp"
+#include <vector>
+
 
 class Card{
     unsigned int _cost;
     std::string _name;
-    Effect _effect;
-    
+    vector<int> _effect;    // Create an empty Vector which will contain all the required variables for his effect.
+
 public:
 
     ///Constructors
-    Card (unsigned int cost, std::string name, Effect effect):
-                            _cost(cost), _name(name), _effect(effect){};
-    
+    Card(unsigned int cost = 0, std::string name = "No name" , vector<int> effect = {0}):
+            _cost(cost), _name(name), _effect(effect){};
+
     ///Getters
-    unsigned int getCost(){return _cost;};
-    Effect getEffect(){return _effect;};
-    std::string getName(){return _name;};
+    unsigned int getCost(){return _cost;}
+    int getEffect(){return _effect[0];}
+    std::string getName(){return _name;}
+
+    ///Methodes
+    virtual void print() {std::cout << _name;}
 
 };
 
-#endif //_CARD_COMMON_HPP
+#endif //_CARD_COMMON
