ead732ed303ed63c4f03232ffb18a901ec8ab5a2
NBaudoux <nbaudoux@ulb.ac.be>
2016-02-14 13:32:41 +0000

    Add list of monster cards for tests

--- include/common/effect.hpp
+++ include/common/effect.hpp
@@ -3,8 +3,10 @@
 
 #define listSize 32  //~ May be changed, I estimated that 32 was enough
 
+#include <vector>
+
 struct Effect {
-    
+    std::vector<int> params;
 };
 
 #endif //_EFFECT_COMMON_HPP
