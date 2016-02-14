eed96fbb7af3342cd3eacf0ea2e02a2ca9c72da9
Amuranov <Amuranov@users.noreply.github.com>
2016-02-14 15:05:26 +0100

    Update monster.hpp

--- include/common/monster.hpp
+++ include/common/monster.hpp
@@ -1,8 +1,7 @@
 #ifndef MONSTER_HPP
 #define MONSTER_HPP
 
-
-#include "card.hpp"
+#include "Card.hpp"
 
 
 ///Monster card : One of the 2 playables card
@@ -13,23 +12,21 @@ class Monster : public Card
 
 public:
 
-	///TODO
 	///Constructors
-	/* Depend on Card class */
+    Monster(unsigned int cost = 0 , std::string name = "No name", vector<int> effect = {0},
+             unsigned int attack = 1, unsigned int health = 1): Card(cost, name, effect),
+              _attack(attack), _health(health){};
 
     ///Getters
-    inline unsigned int getHealth(){return _health;};
-    inline unsigned int getAttack(){return _attack;};
+    inline unsigned int getHealth(){return _health;}
+    inline unsigned int getAttack(){return _attack;}
 
     ///Setters
-	inline void setHealth(unsigned int LP) {_health = LP;};	
-	inline void setAttack(unsigned int attack) {_attack = attack;};
+	inline void setHealth(unsigned int LP) {_health = LP;}
+	inline void setAttack(unsigned int attack) {_attack = attack;}
 
 	///Methods
-	inline bool hasEffect() const {return _effect != 0;};
-
-
-
+	inline bool hasEffect() {return (this->getEffect()!= 0);}
 };
 
 
