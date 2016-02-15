5f4d3671cafa12845cabbb66311d12fdafeb341d
Alexis Reynouard <areynoua@ulb.ac.be>
2016-02-15 16:32:13 +0100

    Add CientDatabase and ServerDatabase classes

---
+++ include/server/database.hpp
@@ -0,0 +1,17 @@
+#ifndef _SERVER_DATABASE_SERVER_HPP
+#define _SERVER_DATABASE_SERVER_HPP
+
+#include "../common/database.hpp"
+
+/// Interface to the server database.
+class DatabaseServer : public Database
+{
+    /// Default relative path to sqlite3 file
+    static constexpr char FILENAME[] = "../ressources/server/database.db";
+public:
+    /// Constructor
+    /// \param filename: relative path to sqlite3 file.
+    explicit DatabaseServer(std::string filename = FILENAME);
+};
+
+#endif //_DATABASE_SERVER_HPP
