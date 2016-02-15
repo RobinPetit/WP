487bdbaa23a5436b3b0a085c3d8c0e5873550fc9
StanIsAdmin <stansevillista@gmail.com>
2016-02-15 17:20:35 +0100

    Add containers for the player's cards and methods for moving them

--- include/server/Player.hpp
+++ include/server/Player.hpp
@@ -15,9 +15,29 @@ class Player
         /// Destructor.
         ~Player() = default;
 
+        void cardPick(unsigned amount=0); ///Move the card on top of the deck to the player's hand
+        void cardPlaceOnBoard(int handIndex); ///Move the card at handIndex from the player's hand to the board
+        void cardDiscardFromHand(int handIndex); ///Move the card at handIndex from the player's hand to the bin
+        void carDiscardFromBoard(int boardIndex); ///Move the card at boardIndex from the board to the bin
+
     private:
 
-        std::stack<std::unique_ptr<Card>> _CardStack;///< Stack of cards that are to be picked (not usable yet)
+        std::stack<std::unique_ptr<Card>> _cardDeck; ///< Cards that are in the deck (not usable yet)
+        std::vector<std::unique_ptr<Card>> _cardHand; ///< Cards that are in the player's hand (usable)
+        std::vector<std::unique_ptr<Card>> _cardBoard; ///< Cards that are on the board (usable for attacks)
+        std::vector<std::unique_ptr<Card>> _cardBin; ///< Cards that are discarded (dead creatures, used spells)
+
+        struct _constraints
+        {
+            unsigned cardPickAmount=1;
+            unsigned handSizeMaxLimit=NULL;
+        };
+
+        struct _turnData
+        {
+            unsigned cardsPicked=0;
+            unsigned cardsPutOnBoard=0;
+        };
 };
 
 
--- src/server/Player.cpp
+++ src/server/Player.cpp
@@ -1,2 +1,28 @@
-#include <Player.hpp>
+#include <server/Player.hpp>
 
+void Player::cardPick(unsigned amount)
+{
+    while (not _cardDeck.empty() and amount>0)
+    {
+        amount--;
+        _cardHand.push_back(_cardDeck.pop());
+    }
+}
+
+void Player::cardPlaceOnBoard(int handIndex)
+{
+    _cardBoard.push_back(_cardHand.at(handIndex));
+    _cardHand.erase(handIndex);
+}
+
+void Player::cardDiscardFromHand(int handIndex)
+{
+    _cardBin.push_back(_cardHand.at(handIndex));
+    _cardHand.erase(handIndex);
+}
+
+void Player::cardDiscardFromBoard(int boardIndex)
+{
+    _cardBin.push_back(_cardBoard.at(boardIndex));
+    _cardBoard.erase(boardIndex);
+}
