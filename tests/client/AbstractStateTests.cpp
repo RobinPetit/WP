717c07365803bea4a9ee8ae4ad3d609a853f626e
TheoVerhelst <tverhels@ulb.ac.be>
2016-02-17 17:04:39 +0100

    Add unit test for AbstractState

--- tests/client/AbstractStateTests.cpp
+++ tests/client/AbstractStateTests.cpp
@@ -1,9 +1,40 @@
 #include <boost/test/unit_test.hpp>
 #include "client/AbstractState.hpp"
+#include "client/StateStack.hpp"
+
+class TestState : public AbstractState
+{
+	public:
+		TestState(StateStack& stateStack):
+			AbstractState(stateStack),
+			_fHasBeenCalled{false},
+			_gHasBeenCalled{false}
+		{
+			addAction("f", &TestState::f);
+			addAction("g", &TestState::g);
+		}
+
+		bool _fHasBeenCalled, _gHasBeenCalled;
+
+	private:
+		void f()
+		{
+			_fHasBeenCalled = true;
+		}
+
+		void g()
+		{
+			_gHasBeenCalled = true;
+		}
+};
 
 struct AbstractStateTestsFixture
 {
-    AbstractStateTestsFixture()
+	StateStack stack;
+	TestState testState;
+
+    AbstractStateTestsFixture():
+		testState{stack}
     {
     }
 
@@ -14,10 +45,12 @@ struct AbstractStateTestsFixture
 
 BOOST_FIXTURE_TEST_SUITE(AbstractStateTests, AbstractStateTestsFixture)
 
-BOOST_AUTO_TEST_CASE(MyMethod)
+BOOST_AUTO_TEST_CASE(MenuCallback)
 {
-    BOOST_CHECK_EQUAL(1 + 1, 2);
-    BOOST_CHECK(1 < 2);
+	testState.handleInput("0");
+	BOOST_CHECK(testState._fHasBeenCalled);
+	testState.handleInput("1");
+	BOOST_CHECK(testState._gHasBeenCalled);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
