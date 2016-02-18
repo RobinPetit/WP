4b6f4ae8f0ca421d090958dbdecbf357852935ba
Amuranov <Amuranov@users.noreply.github.com>
2016-02-18 18:01:01 +0100

    Update Creature.hpp

--- include/server/Creature.hpp
+++ include/server/Creature.hpp
@@ -7,24 +7,26 @@
 ///Creature card : One of the 2 playables card
 class Creature : public Card
 {
+	std::vector<std::vector<unsigned>> _turnByTurnEffects; 
+    std::vector<std::vector<unsigned>> _endingEffects; 
 	unsigned int _attack, _attackInit;
 	unsigned int _health, _healthInit;
-    std::vector<std::vector<unsigned>> _turnByTurnEffects; 
-    std::vector<std::vector<unsigned>> _endingEffects; 
+
 
 public:
 
 	/// Constructors
-    Creature(unsigned int cost = 0 , std::string name = "No name", std::vector<std::vector<unsigned>> effect = {},
-            unsigned int attack = 1, unsigned int health = 1): Card(cost, name, effect),        //ici
-                _attack(attack), _health(health){};
+    Creature(unsigned int cost = 0 , std::string name = "No name", std::vector<std::vector<unsigned>> instantEffects = {{}},
+    		std::vector<std::vector<unsigned>> turnEffects = {{}}, std::vector<std::vector<unsigned>> endingEffects = {{}},
+            	unsigned int attack = 0, unsigned int health = 1): Card(cost, name, instantEffects),
+            		 _turnByTurnEffects(turnEffects), _endingEffects(endingEffects), _attack(attack), _health(health){};
 
     /// Getters
     inline unsigned int getHealth(){return _health;}
     inline unsigned int getAttack(){return _attack;}
     
     inline std::vector<std::vector<unsigned>> getTurnByTurnEffects() {return _turnByTurnEffects;}
-    inline std::vector<std::vector<unsigned>> getEndingEffects()     {return _endingEffectss;}
+    inline std::vector<std::vector<unsigned>> getEndingEffects()     {return _endingEffects;}
 
     /// Re-Setters
 	inline void resetHealth() {_health = _healthInit;}
@@ -39,7 +41,7 @@ public:
 	/// Methods
 	virtual inline bool isCreature() { return true; } override;
 	virtual inline bool isSpell() { return false; } override;
-	virtual bool hasEffects() { return not (_instantEffects.empty() && _turnByTurnEffects.empty() && _endingEffect.empty()) ;} override;
+	virtual bool hasEffects() { return not (_instantEffects.empty() && _turnByTurnEffects.empty() && _endingEffects.empty()) ;} override;
 
 };
 
