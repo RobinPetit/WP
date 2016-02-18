3f2a4a3477a82fd03bc2cbb8cf39ae687a02d409
Amuranov <Amuranov@users.noreply.github.com>
2016-02-18 13:57:36 +0100

    Update Creature.hpp
    Separating hpp and cpp

--- include/server/Creature.hpp
+++ include/server/Creature.hpp
@@ -37,25 +37,4 @@ public:
 
 };
 
-void Creature::incrHealth(unsigned int LP) {_health+=LP;}
-
-void Creature::decrHealth(unsigned int LP)
-{
-    if(_health > LP){
-        _health -= LP;}
-    else
-        _health = 0;
-
-}
-
-void Creature::incrAttack(unsigned int attack) {_attack+=attack;}
-
-void Creature::decrAttack(unsigned int attack)
-{
-    if(_attack > attack){
-        _attack -= attack;}
-    else
-        _attack = 0;
-}
-
 #endif // CREATURE_SERVER_HPP
