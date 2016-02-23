22a6abcd0f80ce61bf79e2e9eb384bceb23366e7
RobinPetit <robpetit@ulb.ac.be>
2016-02-24 00:06:58 +0100

    Comment NonBlockingInput

--- include/client/NonBlockingInput.hpp
+++ include/client/NonBlockingInput.hpp
@@ -6,9 +6,15 @@
 class NonBlockingInput
 {
 public:
+	/// Constructor
 	NonBlockingInput(bool appendMode=false);
 
+	/// This function is called to know if something is typed on stdin in the \a seconds following seconds
+	/// \param seconds The number of seconds to wait before telling whether or not something was typed
+	/// \return True if data is available (then call receiveStdinData) and false otherwise
 	bool waitForData(double seconds);
+
+	/// This function is called if data was typed to "read" it
 	std::string&& receiveStdinData();
 
 	static constexpr unsigned long long int microSecondsInOneSecond{1000000ULL};
--- src/client/NonBlockingInput.cpp
+++ src/client/NonBlockingInput.cpp
@@ -20,12 +20,18 @@ NonBlockingInput::NonBlockingInput(bool appendMode):
 
 bool NonBlockingInput::waitForData(double seconds)
 {
+	// needed for the select function
 	fd_set descriptorsSet;
+	// clear the set
 	FD_ZERO(&descriptorsSet);
+	// and place STDIN in it
 	FD_SET(stdinFileDescriptor, &descriptorsSet);
+	// precise how long to wait
 	struct timeval waitingDelay;
 	waitingDelay.tv_sec = static_cast<time_t>(seconds);
 	waitingDelay.tv_usec = static_cast<suseconds_t>((seconds - static_cast<int>(seconds)) * microSecondsInOneSecond);
+	// and then wait for an input
+	// select return -1 if error occured (\TODO: handle it), 0 if no data was received and > 0 if data was typed
 	if(select(stdinFileDescriptor+1, &descriptorsSet, nullptr, nullptr, &waitingDelay) == 0)
 		return false;
 	// as stdin is the only file in the set, only itself may have received data
@@ -41,6 +47,7 @@ bool NonBlockingInput::waitForData(double seconds)
 	return true;
 }
 
+// consider the buffer like temporary. Then once it is used, it disappears (like a *real* IO stream buffer).
 std::string&& NonBlockingInput::receiveStdinData()
 {
 	return std::move(_buffer);
