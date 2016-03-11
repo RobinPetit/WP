0c64f9f5b587b7a530c57b162500f4cb62a63e9e
Alexis Reynouard <areynoua@ulb.ac.be>
2016-03-11 05:52:32 +0100

    Proper include

--- src/client/ClientSpell.cpp
+++ src/client/ClientSpell.cpp
@@ -1,4 +1,4 @@
-#include "ClientSpell.hpp"
+#include "client/ClientSpell.hpp"
 
 ClientSpell::ClientSpell(cardId cardIdentifier, const std::string& name, int cost, const std::string& description) :
 	ClientCard(cardIdentifier, name, cost, description) {}
