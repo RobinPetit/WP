b4f88d578510c7fb20d95d56de354fa65255fe24
TheoVerhelst <tverhels@ulb.ac.be>
2016-03-15 18:02:37 +0100

    Add basic GUI files
    This commit adds some files that are needed for the GUI part of the
    client.
    
    This commit also create a new class, AbstractApplication, which is
    derived into GuiApplication and TerminalApplication. These classes hold
    the variables that are necessary for the whole client, but separate them
    between terminal and GUI.

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,47 +1,19 @@
-/**
-	client main file (entry point)
-**/
+/// Client main file (entry point).
 
 // std-C++ headers
-#include <iostream>
-#include <cstdlib>
-// WizardPoker heaaders
-#include "client/StateStack.hpp"
-#include "client/Terminal/states/TerminalHomeState.hpp"
-#include "client/sockets/Client.hpp"
+#include <memory>
+// WizardPoker headers
+#include "client/Terminal/TerminalApplication.hpp"
+#include "client/Gui/GuiApplication.hpp"
 
-int main(int argc, char* argv[])
+int main(/* int argc, char* argv[] */)
 {
-	bool noGui{true};
-	// TODO: once the GUI is implemented, the ligne above should be:
-	// bool noGui{false};
-	if(argc > 1 and std::string(argv[1]) == "--no-gui")
-		noGui = true;
-	Client client;
-	StateStack stack(client);
-	if(noGui)
-		stack.push<TerminalHomeState>();
+	std::unique_ptr<AbstractApplication> application;
+	// TODO: uncomment the following line when the GUI is working
+	if(true /* argc > 1 and std::string(argv[1]) == "--no-gui" */)
+		application.reset(new TerminalApplication());
 	else
-	{
-		//stack.push<GuiHomeState>();
-	}
+		application.reset(new GuiApplication());
 
-	while(not stack.isEmpty())
-	{
-		if(noGui)
-			system("clear");
-		stack.display();
-		// Is this try/catch really useful? We do nothing when an exception
-		// is caught, the behavior is almost the same as without try/catch.
-		try
-		{
-			stack.handleInput();
-		}
-		catch(std::runtime_error& error)
-		{
-			std::cerr << "Exception caught: " << error.what();
-			return -1;
-		}
-	}
-	return 0;
+	return application->play();
 }
