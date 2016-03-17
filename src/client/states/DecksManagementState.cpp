469d133c6b3653f791445ed8c087dcf89d7807a7
Alexis Reynouard <areynoua@ulb.ac.be>
2016-03-17 16:17:11 +0100

    Use of database in DecksManagementState (client)
    Use of database (through client new methods)
    
    Great new: with this commit the client is compilable again and the database is
               fully integrated/embedded, I don't know

--- src/client/states/DecksManagementState.cpp
+++ src/client/states/DecksManagementState.cpp
@@ -1,5 +1,6 @@
 // std-C++ headers
 #include <iostream>
+#include <cassert>
 // WizardPoker headers
 #include "client/states/DecksManagementState.hpp"
 #include "common/CardData.hpp"
@@ -118,7 +119,10 @@ cardId DecksManagementState::askForReplacingCard(std::size_t deckIndex)
 	for(const auto& card : _cardsCollection)
 		std::cout << static_cast<std::size_t>(card) << ". " << card << "\n";
 	std::cout << "Which card do you want to put in you deck? ";
-	cardId replacingCard{static_cast<cardId>(askForNumber(0, nbSpells + nbCreatures))};
+	// TODO: even if *for now* database initialization ensure that card ids are consecutive
+	// we shouldn't take this for granted.
+	assert(_client.getMaxCardId() == _client.getNumberOfCards());
+	cardId replacingCard{static_cast<cardId>(askForNumber(0, _client.getNumberOfCards()))};
 
 	// Check if the given card is valid
 	if(not _cardsCollection.contains(replacingCard))
