e8e5924a4b0abe25a92ec66482c45c0c672e68c9
Amuranov <Amuranov@users.noreply.github.com>
2016-03-20 16:33:45 +0100

    Create cardGUI.cpp
    still a texture for the front until I arrange that.

---
+++ src/client/Gui/cardGUI.cpp
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "CardGui.hpp"
+
+
+using namespace std;
+using namespace sf;
+
+CardGUI::CardGUI()
+{
+    if (!_backTexture.loadFromFile("back.png"))
+        {
+            std::cout << "Error back loading" ;
+        }// Standarde protocole SFML 2.0 error ( CF documentations )
+    _frontTexture.setSmooth(true); // enable smooth borders
+
+    if (!_frontTexture.loadFromFile("front.png"))
+        {
+            std::cout << "Error front loading" ;
+        }
+    _frontTexture.setSmooth(true);
+
+    _backView.setTexture(_backTexture);
+    _frontView.setTexture(_frontTexture);
+}
