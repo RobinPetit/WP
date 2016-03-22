16503f07a953e41296774b5e2e7ad2d6b085f578
Amuranov <Amuranov@users.noreply.github.com>
2016-03-22 23:20:23 +0100

    Major update
    Declaring const for the graphic interface, it's arbitrary, we can still change it.

--- include/client/Gui/cardGUI.hpp
+++ include/client/Gui/cardGUI.hpp
@@ -1,32 +1,52 @@
 #ifndef CARDGUI_HPP
 #define CARDGUI_HPP
 
-#include <SFML/Graphics.hpp>
-#include <iostream>
 
 class CardGUI
 {
 private:
+    // name,cost, and effects
+    sf::Text _effects;
+    sf::Text _name;
+    sf::Text _cost;
 
-    // Back view texture and sprite
-    sf::Sprite _backView;
+    //loading characters font
+    sf::Font _font;
+    // Arbitrary shape
+    int const LENGTH = 360;
+    int const WIDTH  = 240;
+
+    int const NAME_POSITION_X = 24;
+    int const NAME_POSITION_Y = 36;
+
+    int const EFFECTS_POSITION_X = 10;
+    int const EFFECTS_POSITION_Y = 28;
+
+    int const CHAR_SIZE = 20;
+
+    /* Back view texture and sprite only, because the front
+    one depends on if it's a spell or creature card*/
+    sf::RectangleShape _backView;
     sf::Texture _backTexture;
 
-    // Front view texture and sprite
-    sf::Sprite _frontView;
-    sf::Texture _frontTexture;
-    
-    // Needed attributs
-    std::vector<EffectParamsCollection> _effects;  // Effects
-    int _cost;
+    sf::RectangleShape _picture;
+    sf::Texture _pictureTexture;
+
 
 public:
-    CardGUI();
+    CardGUI(std::string effects, std::string name, std::string cost);
 
     /// Getters
-    sf::Sprite getBack() {return _backView;}
-    sf::Sprite getFront() {return _frontView;}
+    inline sf::Text   getEffects() {return _effects;}
+    inline sf::Text   getName() {return _name;}
+    inline sf::Text   getCost() {return _cost;}
 
+    /// Setters
+    void setCost(std::string cost) {_cost.setString(cost);}
+
+    /// Others
+    void drawBack();
+    void virtual drawFront() = 0;
 };
 
 
