57555dd75fe48b3b1381fd4acdd14f546178b333
Amuranov <Amuranov@users.noreply.github.com>
2016-03-22 23:18:47 +0100

    Majors changes
    This class became abstract, creature and spell will inherite from this one.
    CardGUI is not finished yet, it will be tomorrow with spell and monsters too.
    
    It's already here so you have a preview

--- src/client/Gui/cardGUI.cpp
+++ src/client/Gui/cardGUI.cpp
@@ -1,21 +1,34 @@
 #include "client/Gui/CardGui.hpp"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
 
-using namespace sf;
-
-CardGUI::CardGUI()
+CardGUI::CardGUI(std::string effects, std::string name, std::string cost)
 {
-    if (!_backTexture.loadFromFile("back.png"))
-        {
-            std::cout << "Error back loading" ;
-        }// Standarde protocole SFML 2.0 error ( CF documentations )
-    _frontTexture.setSmooth(true); // enable smooth borders
+    _backTexture.loadFromFile("back.png");
+    _backTexture.setSmooth(true); // enable smooth borders
+    _backView.setSize(sf::Vector2f(WIDTH, LENGTH));
+    _backView.setTexture(&_backTexture);
 
-    if (!_frontTexture.loadFromFile("front.png"))
-        {
-            std::cout << "Error front loading" ;
-        }
-    _frontTexture.setSmooth(true);
+    _font.loadFromFile("TrueLies.ttf"); // loading chosen font
 
-    _backView.setTexture(_backTexture);
-    _frontView.setTexture(_frontTexture);
+    //complete cost, name and effects
+    _name.setString(name + "\t" + cost);_name.setColor(sf::Color::Red);
+    _name.setFont(_font);
+    _name.setPosition(sf::Vector2f(WIDTH/NAME_POSITION_X, LENGTH/NAME_POSITION_Y));
+
+    _effects.setString(effects);_effects.setColor(sf::Color::Black);
+    _effects.setFont(_font);_effects.setCharacterSize(CHAR_SIZE);
+    _effects.setPosition(sf::Vector2f(WIDTH/EFFECTS_POSITION_X, (LENGTH/NAME_POSITION_Y)*EFFECTS_POSITION_Y));
+
+    //~ TODO ADDING PICTURE TO CONSTRUCTOR
+
 }
+CardGUI::void drawBack()
+{
+    /*Just drawing the back of the cards without any other element */
+    _backView.draw()
+}
+
+
+
