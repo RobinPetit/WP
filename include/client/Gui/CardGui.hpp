5a2a9169a6513b4df2469debd44b967c4805bb9b
TheoVerhelst <tverhels@ulb.ac.be>
2016-03-23 15:44:48 +0100

    Complete documentation of GUI cards classes

--- include/client/Gui/CardGui.hpp
+++ include/client/Gui/CardGui.hpp
@@ -13,10 +13,10 @@ public:
 	/// Destructor.
 	virtual ~CardGui() = default;
 
-	/// Setters
+	/// Setw the cost of the card.
 	void setCost(int cost);
 
-	/// Set which side of the card is drawn when the card is displayed.
+	/// Setw which side of the card is drawn when the card is displayed.
 	/// \param true to show the front (with the image, title, ...), false to
 	/// show the back.
 	void setShownSide(bool showFront);
@@ -26,16 +26,21 @@ public:
 	void setPosition(const sf::Vector2f& position);
 	const sf::Vector2f& getPosition() const;
 
-	/// Draw the object to a render target.
+	/// Draws the object to a render target.
 	virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
 
 protected:
+	/// The global size of the card, in pixels.
 	static const sf::Vector2f SIZE;
 
+	/// The main shape that contains the texture.
 	sf::RectangleShape _picture;
+
+	/// The texture displayed in front of the card.
 	sf::Texture _pictureTexture;
 
-	/// Ease of use method.
+	/// Ease of use method. It configures the card with the right font, position
+	/// , text size and color.
 	void setupText(sf::Text& text, const std::string& string, const sf::Vector2f& position) const;
 
 private:
@@ -51,10 +56,16 @@ private:
 	/// Text font.
 	sf::Font _font;
 
-	// Arbitrary shape
+	/// Position of the name relatively to the card.
 	static const sf::Vector2f NAME_POSITION;
+
+	/// Position of the cost relatively to the card.
 	static const sf::Vector2f COST_POSITION;
+
+	/// Position of the description relatively to the card.
 	static const sf::Vector2f DESCRIPTION_POSITION;
+
+	/// Character size.
 	static constexpr std::size_t CHAR_SIZE = 20;
 
 	/// Current position of the card.
@@ -64,7 +75,10 @@ private:
 	/// showing the back.
 	bool _showFront;
 
+	/// Shape that will contains the image of the back side.
 	sf::RectangleShape _backView;
+
+	/// Texture of the back side.
 	sf::Texture _backTexture;
 };
 
--- include/client/Gui/CreatureGui.hpp
+++ include/client/Gui/CreatureGui.hpp
@@ -10,23 +10,37 @@ public:
 	/// Constructor.
 	CreatureGui(const std::string& name, const std::string& description, int cost, int attack, int health, int shield, int shieldType);
 
-	/// Setters
+	/// Sets the attack.
 	void setAttack(int value);
+
+	/// Sets the health.
 	void setHealth(int value);
+
+	/// Sets the shield.
 	void setShield(int value);
 
-	/// Draw the object to a render target.
+	/// Draws the object to a render target.
 	virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
 
 private:
 	int _shieldType;// TODO use this
 
+	/// Graphical text for the attack.
 	sf::Text _attackText;
+
+	/// Graphical text for the health.
 	sf::Text _healthText;
+
+	/// Graphical text for the shield.
 	sf::Text _shieldText;
 
+	/// Position of the attack text.
 	static const sf::Vector2f ATTACK_POSITION;
+
+	/// Position of the health text.
 	static const sf::Vector2f HEALTH_POSITION;
+
+	/// Position of the shield text.
 	static const sf::Vector2f SHIELD_POSITION;
 };
 
