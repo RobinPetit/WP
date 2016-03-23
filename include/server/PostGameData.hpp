838422d6b4154ca60835aca02dccd4b7aa7d4832
Alexis Reynouard <areynoua@ulb.ac.be>
2016-03-24 00:16:18 +0100

    Consistent use of tabswq

--- include/server/PostGameData.hpp
+++ include/server/PostGameData.hpp
@@ -9,14 +9,14 @@ struct PostGameData
 {
 	bool opponentInDaClub;
 	bool playerStarted;
-    bool playerTookDamage;
+	bool playerTookDamage;
 	bool playerWon;
 	bool playerQuit;
 
 	cardId unlockedCard;
 
-    int remainingHealth;
-    int gameDuration;
+	int remainingHealth;
+	int gameDuration;
 };
 
 #endif  // _POST_GAME_DATA_SERVER_HPP_
