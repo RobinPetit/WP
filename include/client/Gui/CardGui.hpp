053ff57386e84af8591deeb729d02d85b6039801
TheoVerhelst <tverhels@ulb.ac.be>
2016-03-23 14:12:39 +0100

    Use the right type for position and sizes, and use constexpr
    SFML uses float to indicates positions and sizes, so it is better to use
    them also.

--- include/client/Gui/CardGui.hpp
+++ include/client/Gui/CardGui.hpp
@@ -25,16 +25,16 @@ protected:
 	//loading characters font
 	sf::Font _font;
 	// Arbitrary shape
-	int const LENGTH = 360;
-	int const WIDTH  = 240;
+	static constexpr float LENGTH = 360.f;
+	static constexpr float WIDTH  = 240.f;
 
-	int const NAME_POSITION_X = 24;
-	int const NAME_POSITION_Y = 36;
+	static constexpr float NAME_POSITION_X = 24.f;
+	static constexpr float NAME_POSITION_Y = 36.f;
 
-	int const EFFECTS_POSITION_X = 10;
-	int const EFFECTS_POSITION_Y = 28;
+	static constexpr float EFFECTS_POSITION_X = 10.f;
+	static constexpr float EFFECTS_POSITION_Y = 28.f;
 
-	int const CHAR_SIZE = 20;
+	static constexpr std::size_t CHAR_SIZE = 20;
 
 	/* Back view texture and sprite only, because the front
 	one depends on if it's a spell or creature card*/
