4bbfbd5f44920b8910a779db2868f2f023b16d77
StanIsAdmin <stansevillista@gmail.com>
2016-03-24 13:17:25 +0100

    Enhance display, tabs are nice

--- src/server/PostGameData.cpp
+++ src/server/PostGameData.cpp
@@ -16,12 +16,12 @@ PostGameData::PostGameData() :
 
 std::string PostGameData::display()
 {
-	return 	std::string("-opponentInDaClub : ") + std::to_string(opponentInDaClub) +
-			std::string("\n-playerStarted : ") + std::to_string(playerStarted) +
-			std::string("\n-playerTookDamage : ") + std::to_string(playerTookDamage) +
-			std::string("\n-playerWon : ") + std::to_string(playerWon) +
-			std::string("\n-playerQuit : ") + std::to_string(playerQuit) +
-			std::string("\n-unlockedCard : ") + std::to_string(unlockedCard) +
-			std::string("\n-remainingHealth : ") + std::to_string(remainingHealth) +
-			std::string("\n-gameDuration : ") + std::to_string(gameDuration);
+	return 	std::string("\t-opponentInDaClub : ") + std::to_string(opponentInDaClub) +
+			std::string("\n\t-playerStarted : ") + std::to_string(playerStarted) +
+			std::string("\n\t-playerTookDamage : ") + std::to_string(playerTookDamage) +
+			std::string("\n\t-playerWon : ") + std::to_string(playerWon) +
+			std::string("\n\t-playerQuit : ") + std::to_string(playerQuit) +
+			std::string("\n\t-unlockedCard : ") + std::to_string(unlockedCard) +
+			std::string("\n\t-remainingHealth : ") + std::to_string(remainingHealth) +
+			std::string("\n\t-gameDuration : ") + std::to_string(gameDuration);
 }
