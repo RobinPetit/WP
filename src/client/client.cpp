5f3dd57ab246d1b383674d068551320b8a0992a3
RobinPetit <robpetit@ulb.ac.be>
2016-03-25 17:25:19 +0100

    Fix X11 threads problems

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -5,9 +5,20 @@
 // WizardPoker headers
 #include "client/Terminal/TerminalApplication.hpp"
 #include "client/Gui/GuiApplication.hpp"
+#ifdef __linux__
+extern "C"
+{
+# include <X11/Xlib.h>
+}
+#else
+#endif
 
 int main(int argc, char* argv[])
 {
+#ifdef __linux__
+	XInitThreads();
+#else
+#endif
 	std::unique_ptr<AbstractApplication> application;
 	// TODO for now we must give --gui, but eventually this will be inverted,
 	// and we'll have to give --no-gui.
