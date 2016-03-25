90481c2d21ecaebf4a2ef0061fc584658b16ecc9
TheoVerhelst <tverhels@ulb.ac.be>
2016-03-25 22:53:00 +0100

    Begin the implementation of GuiDecksManagementState

--- include/client/Gui/states/GuiDecksManagementState.hpp
+++ include/client/Gui/states/GuiDecksManagementState.hpp
@@ -1,9 +1,19 @@
 #ifndef _GUI_DECKS_MANAGEMENT_STATE_CLIENT_HPP
 #define _GUI_DECKS_MANAGEMENT_STATE_CLIENT_HPP
 
+// External headers
+#include <TGUI/VerticalLayout.hpp>
+#include <TGUI/Widgets/ListBox.hpp>
+#include <TGUI/Widgets/Grid.hpp>
+#include <TGUI/Widgets/Panel.hpp>
+#include <TGUI/Widgets/Scrollbar.hpp>
+#include <TGUI/Widgets/Label.hpp>
 // WizardPoker headers
 #include "client/Gui/GuiAbstractState.hpp"
 #include "client/states/AbstractDecksManagementState.hpp"
+#include "client/Gui/CardWidget.hpp"
+#include "client/Gui/states/ButtonData.hpp"
+#include "common/Identifiers.hpp"
 
 /// Final class for decks management with the GUI.
 class GuiDecksManagementState : public GuiAbstractState, public AbstractDecksManagementState
@@ -11,6 +21,37 @@ class GuiDecksManagementState : public GuiAbstractState, public AbstractDecksMan
 	public:
 		/// Constructor.
 		GuiDecksManagementState(Context& context);
+
+	private:
+		const std::vector<ButtonData<GuiDecksManagementState>> _buttons;
+		tgui::VerticalLayout::Ptr _buttonsLayout;
+		tgui::ListBox::Ptr _decksListBox;
+		tgui::Grid::Ptr _cardGrid;
+		tgui::Panel::Ptr _cardPanel;
+		tgui::Scrollbar::Ptr _scrollbar;
+		std::vector<std::pair<cardId, CardWidget::Ptr>> _cardsCollection;
+		tgui::Label::Ptr _hintLabel;
+
+		/// This variable determines the state of the current operation: if
+		/// true, the deck is displayed in the grid and we are waiting for the
+		/// user to click on a card, and if false, the card collection is
+		/// displayed and the clicked card will replace the one previously
+		/// clicked when this variable was true.
+		bool chooseCardFromDeck;
+
+		static constexpr std::size_t GRID_WIDTH = 2;
+
+		static constexpr float PADDING{30.f};
+
+	private:
+		/// Callback called when the user clicks on a card.
+		void onCardClick(cardId id);
+
+		void selectDeck();
+
+		void removeDeck();
+
+		void createDeck();
 };
 
 #endif  // _GUI_DECKS_MANAGEMENT_STATE_CLIENT_HPP
--- src/client/Gui/states/GuiDecksManagementState.cpp
+++ src/client/Gui/states/GuiDecksManagementState.cpp
@@ -3,6 +3,58 @@
 GuiDecksManagementState::GuiDecksManagementState(Context& context):
 	AbstractState(context),
 	GuiAbstractState(context),
-	AbstractDecksManagementState(context)
+	AbstractDecksManagementState(context),
+	_buttons
+	{
+		{&GuiDecksManagementState::createDeck, "Create a new deck"},
+		{&GuiDecksManagementState::removeDeck, "Remove the selected deck"},
+		{&GuiDecksManagementState::backMainMenu, "Back to main menu"}
+	},
+	_buttonsLayout{std::make_shared<tgui::VerticalLayout>()},
+	_decksListBox{std::make_shared<tgui::ListBox>()},
+	_cardGrid{std::make_shared<tgui::Grid>()},
+	_cardPanel{std::make_shared<tgui::Panel>()},
+	_scrollbar{std::make_shared<tgui::Scrollbar>()},
+	_cardsCollection{},
+	_hintLabel{std::make_shared<tgui::Label>()},
+	chooseCardFromDeck{true}
+{
+	auto windowWidth(tgui::bindWidth(*_context.gui));
+	auto windowHeight(tgui::bindHeight(*_context.gui));
+
+	makeTitle("Decks management");
+
+	_cardPanel->setSize(GRID_WIDTH * CardGui::getSize().x, windowHeight - 120.f);
+	_cardPanel->setPosition(windowWidth - tgui::bindWidth(_cardPanel) - PADDING, 100.f);
+	_cardPanel->setBackgroundColor(sf::Color::Transparent);
+	_context.gui->add(_cardPanel);
+
+	_decksListBox->setPosition(PADDING, 100.f);
+	_decksListBox->setSize(windowWidth - tgui::bindWidth(_cardPanel) - 2.f * PADDING, windowHeight * 0.6f - 120.f);
+	_context.gui->add(_decksListBox);
+	for(const auto& deck : _decks)
+		_decksListBox->addItem(deck.getName());
+
+	_buttonsLayout->setPosition(PADDING, tgui::bindBottom(_decksListBox));
+	_buttonsLayout->setSize(tgui::bindWidth(_decksListBox), windowHeight - tgui::bindHeight(_decksListBox) - 120.f);
+	setupButtons(_buttons, std::static_pointer_cast<tgui::Container>(_buttonsLayout));
+	_context.gui->add(_buttonsLayout);
+
+	registerRootWidgets({_cardPanel, _decksListBox, _buttonsLayout});
+}
+
+void GuiDecksManagementState::onCardClick(cardId id)
+{
+}
+
+void GuiDecksManagementState::selectDeck()
+{
+}
+
+void GuiDecksManagementState::removeDeck()
+{
+}
+
+void GuiDecksManagementState::createDeck()
 {
 }
