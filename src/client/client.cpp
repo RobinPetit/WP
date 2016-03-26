ebc59c696f8cdc9ea0caa1a3fe681f86b427dcbf
TheoVerhelst <tverhels@ulb.ac.be>
2016-03-26 15:03:32 +0100

    Improve command line option support
    Now the client print an error message when wrong options are given, and
    the option --help and -h has been added.

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -4,10 +4,46 @@
 #include "client/Terminal/TerminalApplication.hpp"
 #include "client/Gui/GuiApplication.hpp"
 
+static void printUsage(const std::string& programName)
+{
+	std::cerr << "Usage: " << programName << " [-h] [--no-gui]" << std::endl;
+}
+
+static void printHelp(const std::string& programName)
+{
+	printUsage(programName);
+	std::cerr << "\n\
+Fantasy multiplayer cards game\n\
+\n\
+Optional arguments:\n\
+	-h, --help  show this help message and exit\n\
+	--no-gui    use the terminal version rather than the GUI\n\
+";
+}
+
+static int error(const std::string& programName, const std::string& message)
+{
+	std::cerr << programName << ": error: " << message << std::endl;
+	printUsage(programName);
+	return 1;
+}
+
 int main(int argc, char* argv[])
 {
-	if(argc > 1 and std::string(argv[1]) == "--no-gui")
-		return TerminalApplication().play();
+	int returnValue{0};
+	if(argc > 2)
+		returnValue = error(argv[0], "too many arguments");
+	else if(argc == 2)
+	{
+		const std::string arg{argv[1]};
+		if(arg == "--no-gui")
+			returnValue = TerminalApplication().play();
+		else if(arg == "-h" or arg == "--help")
+			printHelp(argv[0]);
+		else
+			returnValue = error(argv[0], "unrecognized argument: " + arg);
+	}
 	else
-		return GuiApplication().play();
+		returnValue = GuiApplication().play();
+	return returnValue;
 }
