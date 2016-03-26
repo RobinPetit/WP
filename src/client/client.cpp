b928864291aa184895880624a1dda66cd09aa6f2
TheoVerhelst <tverhels@ulb.ac.be>
2016-03-26 14:34:42 +0100

    Change the command line option --gui to --no-gui
    In #91 we stated that the command line options for this programm will be
    --no-gui for running the terminal version, and no option for running the
    graphical version. I used instead --gui at first because we were using
    more often the terminal version, but since the GUI is well deployed,
    this would be better to use --no-gui as stated at first.
    
    This commit also allocates the Application object on the stack rather
    than on the heap, this is shorter and faster (no virtual call).

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,20 +1,13 @@
 /// Client main file (entry point).
 
-// std-C++ headers
-#include <memory>
 // WizardPoker headers
 #include "client/Terminal/TerminalApplication.hpp"
 #include "client/Gui/GuiApplication.hpp"
 
 int main(int argc, char* argv[])
 {
-	std::unique_ptr<AbstractApplication> application;
-	// TODO for now we must give --gui, but eventually this will be inverted,
-	// and we'll have to give --no-gui.
-	if(argc > 1 and std::string(argv[1]) == "--gui")
-		application.reset(new GuiApplication());
+	if(argc > 1 and std::string(argv[1]) == "--no-gui")
+		return TerminalApplication().play();
 	else
-		application.reset(new TerminalApplication());
-
-	return application->play();
+		return GuiApplication().play();
 }
